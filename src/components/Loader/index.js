c553e4ae767865dd7aaeab7e63ad27c15c620c71
Rafael Pelle <rafael.pelle@supero.com.br>
2020-03-11 15:25:58 -0300

    Implements the loading feedback

---
+++ src/components/Loader/index.js
@@ -0,0 +1,6 @@
+import React from 'react'
+import { ActivityIndicator } from 'react-native'
+
+export default function Loader() {
+  return <ActivityIndicator style={{ margin: 30 }} color='#999' />
+}
--- src/pages/Feed/index.js
+++ src/pages/Feed/index.js
@@ -4,12 +4,14 @@ import { View, FlatList } from 'react-native'
 import axiosClient from '../../services/axiosClient'
 import { styles } from './styles'
 import Post from '../../components/Post'
+import Loader from '../../components/Loader'
 
 export default function Feed({ navigation }) {
   const [feedData, setFeedData] = useState([])
   const [limit, setLimit] = useState(5)
   const [page, setPage] = useState(1)
   const [total, setTotal] = useState(0)
+  const [loading, setLoading] = useState(false)
 
   useEffect(() => {
     loadFeed()
@@ -18,6 +20,7 @@ export default function Feed({ navigation }) {
   const loadFeed = async () => {
     if (total && page > total) return
 
+    setLoading(true)
     try {
       const response = await axiosClient.get(`feed?_expand=author&_limit=${limit}&_page=${page}`)
       setFeedData([...feedData, ...response.data])
@@ -26,6 +29,7 @@ export default function Feed({ navigation }) {
     } catch (e) {
       console.error(e)
     }
+    setLoading(false)
   }
 
   return (
@@ -33,9 +37,10 @@ export default function Feed({ navigation }) {
       <FlatList
         data={feedData}
         keyExtractor={(post) => String(post.id)}
-        renderItem={({ item }) => <Post postData={item} />}
         onEndReached={() => loadFeed()}
         onEndReachedThreshold={0.1}
+        renderItem={({ item }) => <Post postData={item} />}
+        ListFooterComponent={loading && <Loader />}
       />
     </View>
   )
