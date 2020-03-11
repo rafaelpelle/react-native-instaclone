9bff2490a891ac753b1008139397b315d42e39a9
Rafael Pelle <rafael.pelle@supero.com.br>
2020-03-11 15:52:07 -0300

    Implements pull down to refresh

--- src/pages/Feed/index.js
+++ src/pages/Feed/index.js
@@ -6,30 +6,40 @@ import { styles } from './styles'
 import Post from '../../components/Post'
 import Loader from '../../components/Loader'
 
+const limit = 4
+
 export default function Feed({ navigation }) {
   const [feedData, setFeedData] = useState([])
-  const [limit, setLimit] = useState(5)
   const [page, setPage] = useState(1)
   const [total, setTotal] = useState(0)
   const [loading, setLoading] = useState(false)
+  const [refreshing, setRefreshing] = useState(false)
 
   useEffect(() => {
     loadFeed()
   }, [])
 
-  const loadFeed = async () => {
+  const loadFeed = async (pageToLoad = page, shoudRefresh = false) => {
     if (total && page > total) return
-
     setLoading(true)
     try {
-      const response = await axiosClient.get(`feed?_expand=author&_limit=${limit}&_page=${page}`)
-      setFeedData([...feedData, ...response.data])
+      const response = await axiosClient.get(
+        `feed?_expand=author&_limit=${limit}&_page=${pageToLoad}`,
+      )
+      setFeedData(shoudRefresh ? response.data : [...feedData, ...response.data])
       setTotal(Math.floor(response.headers['x-total-count'] / limit))
       setPage(page + 1)
     } catch (e) {
       console.error(e)
     }
     setLoading(false)
+  }
+
+  const handleRefresh = async () => {
+    setRefreshing(true)
+    setFeedData([])
+    await loadFeed(1, true)
+    setRefreshing(false)
   }
 
   return (
@@ -41,6 +51,8 @@ export default function Feed({ navigation }) {
         onEndReachedThreshold={0.1}
         renderItem={({ item }) => <Post postData={item} />}
         ListFooterComponent={loading && <Loader />}
+        onRefresh={handleRefresh}
+        refreshing={refreshing}
       />
     </View>
   )
